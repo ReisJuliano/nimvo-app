d69f6a7d564bf65ef9cfa2510f51f86e14bb6570
ReisJuliano <d.julianoreis@gmail.com>
2026-03-27 09:46:40 -0300

    Corrige redirect para /pdv/

--- routes/tenant.php
+++ routes/tenant.php
@@ -3,8 +3,5 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    ob_start();
-    chdir(public_path('pdv'));
-    include public_path('pdv/login.php');
-    return ob_get_clean();
+    return redirect('/pdv/login.php');
 });
